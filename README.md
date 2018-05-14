d21d985f026a1393abbc544ae99169d607ec612a
Christophe Marchand <christophe@marchand.top>
2018-05-14 11:21:21 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+** dep-tree-maven-plugin **
+
+This plugin generates a text file that contains de dependency-tree, and declares it as an artifact. Hence, when a `mvn deploy` is run, this text file is also deployed.
+
+This allows a [Nexus](https://fr.sonatype.com/nexus-repository-sonatype) Open Source to store dependency informations.  
+Then, a simple script can snif the Nexus repository, read dependency trees information files, and fill in a database. If interested, you may have a look at [dependency-sniffer](https://github.com/cmarchand/dependency-sniffer) which does this, and provides a web client to browse dependencies.
+
+Usually, you use this plugin only when you publish, so on deploy phase. But as it does very simple thinggs, you can use it at each build.
+
+To use this plugin, just add this to your `pom.xml` :
+
+```
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>top.marchand.maven</groupId>
+        <artifactId>dep-tree-maven-plugin</artifactId>
+        <version >1.0.0</version>
+        <executions>
+          <execution>
+            <goals><goal>tree</goal></goals>
+          </execution>
+        </executions>
+      </plugin>
+...
+```
+
--- src/main/java/top/marchand/maven/depTree/DepTreeMojo.java
+++ src/main/java/top/marchand/maven/depTree/DepTreeMojo.java
@@ -1,31 +1,22 @@
-/**
- * Copyright © 2017, Christophe Marchand
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the <organization> nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 package top.marchand.maven.depTree;
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
@@ -85,7 +76,7 @@ public class DepTreeMojo extends AbstractMojo {
     @Parameter( defaultValue = "${project}", readonly = true, required = true )
     private MavenProject project;
     
-     @Parameter( defaultValue = "${session}", readonly = true, required = true )
+    @Parameter( defaultValue = "${session}", readonly = true, required = true )
     private MavenSession session;
 
     /**
@@ -136,7 +127,7 @@ public class DepTreeMojo extends AbstractMojo {
     @Parameter( property = "tokens", defaultValue = "standard" )
     private String tokens;
 
-/**
+    /**
      * A comma-separated list of artifacts to filter the serialized dependency tree by, or <code>null</code> not to
      * filter the dependency tree. The filter syntax is:
      * 
@@ -185,16 +176,12 @@ public class DepTreeMojo extends AbstractMojo {
 
     /**
      * Whether to append outputs into the output file or overwrite it.
-     *
-     * @since 2.2
      */
     @Parameter( property = "appendOutput", defaultValue = "false" )
     private boolean appendOutput;
 
     /**
      * Skip plugin execution completely.
-     *
-     * @since 2.7
      */
     @Parameter( property = "skip", defaultValue = "false" )
     private boolean skip;
@@ -237,7 +224,6 @@ public class DepTreeMojo extends AbstractMojo {
         } catch ( IOException exception ) {
             throw new MojoExecutionException( "Cannot serialise project dependency graph", exception );
         }
-        projectHelper.attachArtifact(project, "txt", "tree", outputFile);
     }
 
     public boolean isSkip() {
